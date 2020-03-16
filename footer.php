934dfbc127f628ff1d422dc342aefb3a99ebef24
Berkay-Cubuk <berkaycubuk@gmail.com>
2020-03-16 13:21:25 +0300

    latest-commit

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-sm">
                     <p class="footer-text">
-                        All rights reserved &copy; 2020
+                        Wordpress &copy; 2020
                     </p>
                 </div>
                 <div class="col-sm">
